517b83f22a65cd042318d03103a854579007b066
sescobar-2020031 <sescobar-2020031@kinal.edu.gt>
2022-05-18 22:18:55 -0600

    Se modificó el modelo de -company-.

--- src/models/company.model.js
+++ src/models/company.model.js
@@ -3,17 +3,14 @@
 
 const mongoose = require('mongoose');
 const companySchema = mongoose.Schema({
-
     name: String,
     username: String,
     password: String,
     email: String,
-    typeCompany: {type: mongoose.Schema.ObjectId, ref: 'TypeCompany'},
-    email: String,
     phone: String,
     role: String,
-    products: [{ type: mongoose.Schema.ObjectId, ref: 'Product' }] 
-
+    typeCompany: {type: mongoose.Schema.ObjectId, ref: 'TypeCompany'} 
 });
 
 module.exports = mongoose.model('Company', companySchema);
+
