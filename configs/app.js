89820be5a03bc288df4df6ff1169b06339e6ba15
rcardenas-2020166 <rcardenas-2020166@kinal.edu.gt>
2022-05-18 23:13:47 -0600

    Se modificó el archivo app.js con modificaciones en las path.

--- configs/app.js
+++ configs/app.js
@@ -9,11 +9,11 @@ const cors = require ('cors');
 
 
 //Importación de las Rutas//
-const userRoutes = require('../src/routes/user.routes');
-const teamRoutes = require('../src/routes/team.routes');
-const tournamentRoutes = require('../src/routes/tournament.routes');
-const journeyRoutes = require('../src/routes/journey.routes');
-
+const companyRoutes = require('../src/routes/company.routes');
+const townshipRoutes = require('../src/routes/township.routes');
+const companyProductRoutes = require('../src/routes/companyProduct.routes');
+const typeCompanyRoutes = require('../src/routes/typeCompany.routes');
+const branchRoutes = require('../src/routes/branch.routes');
 
 //APP -> Servidor HTTP (Express)
 const app = express(); //Creación del Servidor de Express
@@ -26,11 +26,11 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use('/user', userRoutes);
-app.use('/tournament', tournamentRoutes);
-app.use('/team', teamRoutes);
-app.use('/journey', journeyRoutes);
-
+app.use('/company', companyRoutes);
+app.use('/township', townshipRoutes);
+app.use('/companyProduct', companyProductRoutes);
+app.use('/typeCompany', typeCompanyRoutes);
+app.use('/branch', branchRoutes);
 
 //Exportación//
 module.exports = app;
